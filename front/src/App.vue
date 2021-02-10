a906714f7ef6cf9ac15daf0a3ffeac8c429d4ac5
yahaya <sandati.yahaya@epitech.eu>
2021-02-10 12:29:27 +0400

    new

---
+++ front/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <headerbar/>
+  </div>
+</template>
+
+<script>
+import headerbar from './components/headerbar'
+export default {
+  components: {
+    headerbar
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
--- front/src/components/headerbar.vue
+++ front/src/components/headerbar.vue
@@ -3,7 +3,8 @@
     <html>
     <body>
 
-        <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <p>Welcome</p>
 
     </body>
     </html>
@@ -11,10 +12,21 @@
 </template>
 
 <script>
-  export default {
-    
-}
 </script>
 
 <style lang="scss" scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #14e9de;
+}
 </style>
