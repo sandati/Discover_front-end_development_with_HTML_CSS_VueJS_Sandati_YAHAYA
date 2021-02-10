1f8705d91cb574af3e8541260194af392a2bd8d5
yahaya <sandati.yahaya@epitech.eu>
2021-02-10 10:21:17 +0400

    first

---
+++ front/src/components/headerbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <html>
+    <body>
+
+        <h1>Hello World</h1>
+
+    </body>
+    </html>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
